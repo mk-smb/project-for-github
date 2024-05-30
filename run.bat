a16d3f5e6d7a9403f274966b6e840839730c50c0
mk-smb <mayank241332@gmail.com>
2024-05-30 19:20:59 +0530

    tailwindcss-command

---
+++ run.bat
@@ -0,0 +1,1 @@
+npx tailwindcss -i ./project-for-github/input.css -o ./project-for-github/output.css --watch
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["./*.html", "./project-for-github/*.html"],
   theme: {
     extend: {},
   },
